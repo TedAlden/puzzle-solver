7b86a29ad3f0a3dbbced2ec2a98d1eded8041457
Ted <t.aldentempleman@gmail.com>
2024-10-16 01:01:06 +0100

    Add documentation (#8)
    * Docstrings for nqueens.js
    
    * Docstrings for NQueenPuzzle.js
    
    * Docstrings for Board.js

--- src/components/NQueenPuzzle/NQueenPuzzle.js
+++ src/components/NQueenPuzzle/NQueenPuzzle.js
@@ -3,15 +3,27 @@ import Board from '../Board/Board';
 import { solveNQueens } from '../../lib/nqueens';
 import './NQueenPuzzle.css';
 
+/**
+ * Creates an empty chess board.
+ * @param {number} size The size (N) of the N x N chess board
+ * @returns {number[][]} A 2-dimensional array filled with zeros
+ */
 function createBoard (size) {
   return Array(size).fill().map(() => Array(size).fill(0));
 }
 
+/**
+ * Displays the N-Queens puzzle solver.
+ * @returns {React.JSX.Element}
+ */
 function NQueenPuzzle() {
   const [boardSize, setBoardSize] = useState(4);
   const [board, setBoard] = useState(createBoard(boardSize));
   const [solved, setSolved] = useState(false);
 
+  /**
+   * Attempts to solve the N-Queens problem using the board.
+   */
   const solveBoard = () => {
     // Create a copy of the chess board
     const copyBoard = Array.from(board);
@@ -26,6 +38,9 @@ function NQueenPuzzle() {
     }
   }
 
+  /**
+   * Clears the board.
+   */
   const clearBoard = () => {
     // Reset board
     const newBoard = createBoard(boardSize);
@@ -34,6 +49,10 @@ function NQueenPuzzle() {
     setSolved(false);
   }
 
+  /**
+   * Resizes the board based on the board-size input.
+   * @param {*} e Event
+   */
   const resizeBoard = (e) => {
     // Update the board size
     const newSize = Math.max(1, parseInt(e.target.value) || 0);

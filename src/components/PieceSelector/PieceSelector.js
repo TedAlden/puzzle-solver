776c46ffeef019cf13dc1b2aed7cedd6208943fc
Ted <t.aldentempleman@gmail.com>
2024-10-27 13:36:26 +0000

    Implemented piece placement and created a piece selector

---
+++ src/components/PieceSelector/PieceSelector.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import ShapePreview from '../ShapePreview/ShapePreview';
+
+function PieceSelector({
+  shapes,
+  selectedShape,
+  setSelectedShape
+}) {
+  const selectPreviousShape = () => {
+    const currentIndex = shapes.indexOf(selectedShape);
+    const newIndex = (currentIndex - 1 + shapes.length) % shapes.length;
+    setSelectedShape(shapes[newIndex]);
+  }
+
+  const selectNextShape = () => {
+    const currentIndex = shapes.indexOf(selectedShape);
+    const newIndex = (currentIndex + 1) % shapes.length;
+    setSelectedShape(shapes[newIndex]);
+  }
+
+  const rotateShape = () => {
+    const newShape = {...selectedShape};
+    newShape.coords = newShape.coords.map(([x, y]) => [y, -x]);
+    setSelectedShape(newShape);
+  }
+
+  const flipShape = () => {
+    const newShape = {...selectedShape};
+    newShape.coords = newShape.coords.map(([x, y]) => [-x, y]);
+    setSelectedShape(newShape);
+  }
+
+  return (
+    <div className="shapeSelector">
+      <button onClick={flipShape}>Flip</button>
+      <button onClick={selectPreviousShape}>Prev</button>
+      <ShapePreview selectedShape={selectedShape}/>
+      <button onClick={selectNextShape}>Next</button>
+      <button onClick={rotateShape}>Rotate</button>
+    </div>
+  );
+}
+
+export default PieceSelector;
---
+++ src/components/ShapePreview/ShapePreview.js
@@ -0,0 +1,48 @@
+function ShapePreview ({selectedShape}) {
+  const tileSize = 20; // Size of each square tile in shape preview (px)
+  const previewCols = 5; // Width of preview viewport (# of tiles)
+  const previewRows = 5; // Height of preview viewport (# of tiles)
+
+  function centerCoords(shape) {
+    // Find the minimum and maximum x and y values
+    const minX = Math.min(...shape.map(([x, _]) => x * 2)) / 2;
+    const maxX = Math.max(...shape.map(([x, _]) => x * 2)) / 2;
+    const minY = Math.min(...shape.map(([_, y]) => y * 2)) / 2;
+    const maxY = Math.max(...shape.map(([_, y]) => y * 2)) / 2;
+    // Calculate the midpoint
+    const midX = (minX + maxX) / 2;
+    const midY = (minY + maxY) / 2;
+    // Center the shape around the origin
+    return shape.map(([x, y]) => [x - midX, y - midY]);
+  }
+
+  const generateShapeRects = () => {
+    const coords = centerCoords(selectedShape.coords);
+    const tiles = [];
+    coords.forEach(([x, y]) => {
+      tiles.push(
+        <rect
+          key={`${x}-${y}`}
+          x={(x * tileSize) + (previewCols * tileSize * 0.5) - (tileSize * 0.5)}
+          y={(y * tileSize) + (previewRows * tileSize * 0.5) - (tileSize * 0.5)}
+          width={tileSize}
+          height={tileSize}
+          fill={selectedShape.colour}
+        />
+      )
+    });
+    return tiles;
+  }
+
+  return (
+    <svg
+      className="shapePreview"
+      width={previewCols * tileSize}
+      height={previewRows * tileSize}
+    >
+      {generateShapeRects()}
+    </svg>
+  );
+}
+
+export default ShapePreview;

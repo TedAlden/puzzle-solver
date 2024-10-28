ae561161385c7115abd841acd94608b311e3a76c
Ted <t.aldentempleman@gmail.com>
2024-10-28 12:58:14 +0000

    Refactor polysphere puzzle

--- src/components/PolyspherePuzzle/PolyspherePuzzle.js
+++ src/components/PolyspherePuzzle/PolyspherePuzzle.js
@@ -4,19 +4,27 @@ import PolyBoard from '../PolyBoard/PolyBoard';
 import PieceSelector from '../PieceSelector/PieceSelector';
 import pieces from '../../lib/pieces';
 
+const createBoard = (width, height) => (
+  Array(height).fill().map(
+    () => Array(width).fill("")
+  )
+);
 
 function PolyspherePuzzle() {
-  const [board, setBoard] = useState(Array(5).fill().map(() => Array(11).fill("")));
+  const [board, setBoard] = useState(createBoard(5, 11));
   const [shapes, setShapes] = useState(pieces);
   const [selectedShape, setSelectedShape] = useState(shapes[0]);
 
   return (
     <div className="puzzleTwo">
       <h2>The Polysphere Puzzle</h2>
-      <p>This puzzle involves placing unique shapes made of connected spheres
-        onto a 5x11 board. Your goal is to fit all 12 pieces perfectly into the grid.
-        Each shape is made from a different configuration of spheres, and you can use the
-        'Solve' button to find the best way to complete the board.</p>
+      <p>
+        This puzzle involves placing unique shapes made of connected
+        spheres onto a 5x11 board. Your goal is to fit all 12 pieces
+        perfectly into the grid. Each shape is made from a different
+        configuration of spheres, and you can use the 'Solve' button to
+        find the best way to complete the board.
+      </p>
       <PieceSelector
         shapes={shapes}
         selectedShape={selectedShape}

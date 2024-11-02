f87c16fa22b37f36593c2af365a5ff9b0ee27a4a
Charlotte Mackay <77455208+CharlotteM7@users.noreply.github.com>
2024-11-02 14:48:11 +0000

    installed react router for page navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
 import NQueenPuzzle from './components/NQueenPuzzle/NQueenPuzzle';
 import PolyspherePuzzle from './components/PolyspherePuzzle/PolyspherePuzzle';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">   
-        <h2>Welcome to the puzzle solver!</h2>
-      </header>
-      <NQueenPuzzle></NQueenPuzzle>
-      <PolyspherePuzzle></PolyspherePuzzle>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/nqueen">N-Queen Puzzle</Link>
+          <Link to="/polysphere">Polysphere Puzzle</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/nqueen" element={<NQueenPuzzle />} />
+          <Route path="/polysphere" element={<PolyspherePuzzle />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

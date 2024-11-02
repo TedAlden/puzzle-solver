2b43e3b0cb7c670711915d5105dbebd9ff2f0759
Ted <t.aldentempleman@gmail.com>
2024-11-02 16:40:08 +0000

    Home page and no navbar on puzzle pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home/Home';
 import NQueenPuzzle from './components/NQueenPuzzle/NQueenPuzzle';
 import PolyspherePuzzle from './components/PolyspherePuzzle/PolyspherePuzzle';
-import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/nqueen">The N-Queens Puzzle</Link>
-          <Link to="/polysphere">Polysphere Puzzle</Link>
-        </nav>
+    <div className="App">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/nqueen" element={<NQueenPuzzle />} />
           <Route path="/polysphere" element={<PolyspherePuzzle />} />
         </Routes>
-      </div>
-    </Router>
-   
+      </BrowserRouter>
+    </div>
   );
 }
 

9b0ee9e513d6b59469bb6dcff77d8f85b2518f43
Ted <t.aldentempleman@gmail.com>
2024-11-02 18:38:59 +0000

    Switch to MemoryRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import NQueenPuzzle from './components/NQueenPuzzle/NQueenPuzzle';
@@ -8,13 +8,13 @@ import PolyspherePuzzle from './components/PolyspherePuzzle/PolyspherePuzzle';
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <MemoryRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/nqueen" element={<NQueenPuzzle />} />
           <Route path="/polysphere" element={<PolyspherePuzzle />} />
         </Routes>
-      </HashRouter>
+      </MemoryRouter>
     </div>
   );
 }

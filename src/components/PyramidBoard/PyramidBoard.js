0e068e5f7a1c8a64638d114910f0ba7ab1bfef53
Ted <t.aldentempleman@gmail.com>
2024-11-17 13:02:28 +0000

    Refactor separate PyramidBoard

---
+++ src/components/PyramidBoard/PyramidBoard.js
@@ -0,0 +1,63 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+function Piece(props) {
+  return (
+    <mesh {...props}>
+      <sphereGeometry args={[2.5]} />
+      <meshStandardMaterial
+        transparent={true}
+        opacity={0.7}
+        color={props.shapeValue === 0 ? "#ffffff" : "#ff0000"}
+      />
+    </mesh>
+  );
+}
+
+function PyramidBoard({ board }) {
+  const renderPyramid = (pyramid) => {
+    const size = pyramid.length;
+    // Offset y coordinate to center the pyramid at (0, 0, 0)
+    const heightOffset = (size - 1) * 2;
+    const elements = [];
+    for (let i = 0; i < size; i++) {
+      const layerSize = size - i; // i.e. 5 for the first 5x5 layer
+      const y = i * 4 - heightOffset;
+      for (let j = 0; j < layerSize; j++) {
+        for (let k = 0; k < layerSize; k++) {
+          const x = (j - (layerSize - 1) / 2) * 5;
+          const z = (k - (layerSize - 1) / 2) * 5;
+          elements.push(
+            <Piece
+              key={`${j}-${i}-${k}`}
+              position={[x, y, z]}
+              shapeValue={pyramid[i][j][k]}
+            />
+          );
+        }
+      }
+    }
+    return elements;
+  };
+
+  return (
+    <Canvas
+      camera={{
+        position: [25, 25, 25],
+        fov: 50,
+      }}
+    >
+      <ambientLight intensity={Math.PI / 2} />
+      <pointLight position={[0, 20, 0]} decay={0} intensity={Math.PI} />
+      <OrbitControls
+        enablePan={false}
+        target={[0, 0, 0]}
+        enableDamping={true}
+        dampingFactor={0.1}
+      />
+      {renderPyramid(board)}
+    </Canvas>
+  );
+}
+
+export default PyramidBoard;
--- src/pages/PyramidPuzzle/PyramidPuzzle.js
+++ src/pages/PyramidPuzzle/PyramidPuzzle.js
@@ -1,6 +1,6 @@
 import "./PyramidPuzzle.css";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { useEffect, useState } from "react";
+import PyramidBoard from "../../components/PyramidBoard/PyramidBoard";
 
 /**
  * Creates a 3D pyramid puzzle with a given size.
@@ -13,47 +13,14 @@ const createPyramid = (size) =>
     Array.from({ length: size - i }, () => Array(size - i).fill(0))
   );
 
-function Piece(props) {
-  return (
-    <mesh {...props}>
-      <sphereGeometry args={[2.5]} />
-      <meshStandardMaterial
-        transparent={true}
-        opacity={0.7}
-        color={props.shapeValue === 0 ? "#ffffff" : "#ff0000"}
-      />
-    </mesh>
-  );
-}
-
 function PyramidPuzzle() {
-  const pyramid = createPyramid(5);
-  pyramid[0][4][4] = 1;
+  const [board, setBoard] = useState(createPyramid(5));
 
-  const renderPyramid = (pyramid) => {
-    const size = pyramid.length;
-    // Offset y coordinate to center the pyramid at (0, 0, 0)
-    const heightOffset = (size - 1) * 2;
-    const elements = [];
-    for (let i = 0; i < size; i++) {
-      const layerSize = size - i; // i.e. 5 for the first 5x5 layer
-      const y = i * 4 - heightOffset;
-      for (let j = 0; j < layerSize; j++) {
-        for (let k = 0; k < layerSize; k++) {
-          const x = (j - (layerSize - 1) / 2) * 5;
-          const z = (k - (layerSize - 1) / 2) * 5;
-          elements.push(
-            <Piece
-              key={`${j}-${i}-${k}`}
-              position={[x, y, z]}
-              shapeValue={pyramid[i][j][k]}
-            />
-          );
-        }
-      }
-    }
-    return elements;
-  };
+  useEffect(() => {
+    const newPyramid = createPyramid(5);
+    newPyramid[0][4][4] = 1;
+    setBoard(newPyramid);
+  }, []);
 
   return (
     <div className="puzzleThree">
@@ -65,22 +32,7 @@ function PyramidPuzzle() {
         different configuration of spheres, and you can use the <b> Solve </b>
         button to find the best way to complete the board.
       </p>
-      <Canvas
-        camera={{
-          position: [25, 25, 25],
-          fov: 50,
-        }}
-      >
-        <ambientLight intensity={Math.PI / 2} />
-        <pointLight position={[0, 20, 0]} decay={0} intensity={Math.PI} />
-        <OrbitControls
-          enablePan={false}
-          target={[0, 0, 0]}
-          enableDamping={true}
-          dampingFactor={0.1}
-        />
-        {renderPyramid(pyramid)}
-      </Canvas>
+      <PyramidBoard board={board} />
     </div>
   );
 }

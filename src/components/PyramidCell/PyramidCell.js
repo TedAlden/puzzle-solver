60b4519f48493e9d14468a755d79b3808a924a90
Ted <t.aldentempleman@gmail.com>
2024-11-23 20:06:17 +0000

    Refactor PyramidBoard and PyramidCell

--- src/components/PyramidCell/PyramidCell.js
+++ src/components/PyramidCell/PyramidCell.js
@@ -1,33 +1,10 @@
-const COLOURS = {
-  a: "#ff0000",
-  b: "#dd4080",
-  c: "#ee8fbf",
-  d: "#1046bb",
-  e: "#ffff00",
-  f: "#a934cc",
-  g: "#5c00a7",
-  h: "#2ca350",
-  i: "#b64816",
-  j: "#005a1e",
-  k: "#e78e3a",
-  l: "#0091d4",
-};
-
-function PyramidCell({ position, shapeSymbol, isHighlighted, selectedShape }) {
-  const hasShapePlaced = shapeSymbol !== "";
-
-  const colour = hasShapePlaced
-    ? COLOURS[shapeSymbol.toLowerCase()]
-    : isHighlighted
-    ? COLOURS[selectedShape.symbol.toLowerCase()]
-    : "#ffffff";
-
+function PyramidCell({ position, colour, opacity }) {
   return (
     <mesh position={position}>
       <sphereGeometry args={[2.5]} />
       <meshStandardMaterial
         transparent={true}
-        opacity={hasShapePlaced ? 0.9 : 0.5}
+        opacity={opacity}
         color={colour}
       />
     </mesh>

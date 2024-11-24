8996b6c59cf08cfe652b5222a3732016a80df7e9
Ted <t.aldentempleman@gmail.com>
2024-11-24 11:43:13 +0000

    Fix tests for NQueens

--- src/components/QueenBoard/QueenBoard.test.js
+++ src/components/QueenBoard/QueenBoard.test.js
@@ -5,26 +5,36 @@ import QueenBoard from "./QueenBoard";
 
 describe("N-Queens Board Integration Test", () => {
   test("Board array should have queen after clicking on empty cell", () => {
-    const board = [
+    // Initial board with no queens placed
+    const initialBoard = [
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
     ];
-
-    const setBoardMock = jest.fn();
-
+    let board = [...initialBoard];
+    // Mock the onClick method for the cells
+    const handleMouseClickCellMock = jest.fn((row, col) => {
+      const newBoard = board.map((rowArr, rowIndex) =>
+        rowArr.map((cell, colIndex) =>
+          rowIndex === row && colIndex === col ? (cell === 1 ? 0 : 1) : cell
+        )
+      );
+      board = newBoard;
+    });
+    // Render the board
     const { getAllByRole } = render(
-      <QueenBoard board={board} setBoard={setBoardMock} />
+      <QueenBoard
+        board={board}
+        handleMouseClickCell={handleMouseClickCellMock}
+      />
     );
-
     // Simulate click on the first cell (row 0, col 0)
     const cells = getAllByRole("cell");
     const firstCell = cells[0];
     fireEvent.click(firstCell);
-
     // Expect a 1 (queen) in this cell
-    expect(setBoardMock).toHaveBeenCalledWith([
+    expect(board).toEqual([
       [1, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
@@ -33,26 +43,36 @@ describe("N-Queens Board Integration Test", () => {
   });
 
   test("Board array should not have queen after clicking on queen cell", () => {
-    const board = [
+    // Initial board with queen placed in (0, 0)
+    const initialBoard = [
       [1, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
     ];
-
-    const setBoardMock = jest.fn();
-
+    let board = [...initialBoard];
+    // Mock the onClick method for the cells
+    const handleMouseClickCellMock = jest.fn((row, col) => {
+      const newBoard = board.map((rowArr, rowIndex) =>
+        rowArr.map((cell, colIndex) =>
+          rowIndex === row && colIndex === col ? (cell === 1 ? 0 : 1) : cell
+        )
+      );
+      board = newBoard;
+    });
+    // Render the board
     const { getAllByRole } = render(
-      <QueenBoard board={board} setBoard={setBoardMock} />
+      <QueenBoard
+        board={board}
+        handleMouseClickCell={handleMouseClickCellMock}
+      />
     );
-
     // Simulate click on the first cell (row 0, col 0)
     const cells = getAllByRole("cell");
     const firstCell = cells[0];
     fireEvent.click(firstCell);
-
-    // Expect a 0 (no queen) in this cell
-    expect(setBoardMock).toHaveBeenCalledWith([
+    // Expect a 0 (empty) in this cell
+    expect(board).toEqual([
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
@@ -61,72 +81,84 @@ describe("N-Queens Board Integration Test", () => {
   });
 
   test("UI should display queen after clicking on empty cell", async () => {
+    // Initial board with no queens placed
     const initialBoard = [
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
     ];
-
-    // Mock setter
     let board = [...initialBoard];
-    const setBoardMock = jest.fn();
-    setBoardMock.mockImplementation((newBoard) => {
+    // Mock the onClick method for the cells
+    const handleMouseClickCellMock = jest.fn((row, col) => {
+      const newBoard = board.map((rowArr, rowIndex) =>
+        rowArr.map((cell, colIndex) =>
+          rowIndex === row && colIndex === col ? (cell === 1 ? 0 : 1) : cell
+        )
+      );
       board = newBoard;
     });
-
+    // Render the board
     const { getAllByRole, rerender } = render(
-      <QueenBoard board={board} setBoard={setBoardMock} />
+      <QueenBoard
+        board={board}
+        handleMouseClickCell={handleMouseClickCellMock}
+      />
     );
-
-    // Simulate click on the first cell (row 0, col 0)
+    // Simulate click on the first cell (row 0, col 0) to place a queen
     const cells = getAllByRole("cell");
     const firstCell = cells[0];
     fireEvent.click(firstCell);
-
-    // Update the board after placing the queen
-    const updatedBoard = [...board];
-    updatedBoard[0][0] = 1;
-    setBoardMock(updatedBoard);
-    rerender(<QueenBoard board={updatedBoard} setBoard={setBoardMock} />);
-
-    // After clicking, expect a queen to be displayed
+    // Re-render the board
+    rerender(
+      <QueenBoard
+        board={board}
+        handleMouseClickCell={handleMouseClickCellMock}
+      />
+    );
+    // Expect there to now be a queen in the first cell
     await waitFor(() => {
       expect(firstCell).toHaveTextContent("♛");
     });
   });
 
   test("UI should not display queen after clicking on queen cell", async () => {
+    // Initial board with queen placed
     const initialBoard = [
       [1, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
     ];
-
-    // Mock setter
     let board = [...initialBoard];
-    const setBoardMock = jest.fn();
-    setBoardMock.mockImplementation((newBoard) => {
+    // Mock the onClick method for the cells
+    const handleMouseClickCellMock = jest.fn((row, col) => {
+      const newBoard = board.map((rowArr, rowIndex) =>
+        rowArr.map((cell, colIndex) =>
+          rowIndex === row && colIndex === col ? (cell === 1 ? 0 : 1) : cell
+        )
+      );
       board = newBoard;
     });
-
+    // Render the board
     const { getAllByRole, rerender } = render(
-      <QueenBoard board={board} setBoard={setBoardMock} />
+      <QueenBoard
+        board={board}
+        handleMouseClickCell={handleMouseClickCellMock}
+      />
     );
-
-    // Simulate click on the first cell (row 0, col 0)
+    // Simulate click on the first cell (row 0, col 0) to remove the queen
     const cells = getAllByRole("cell");
     const firstCell = cells[0];
     fireEvent.click(firstCell);
-
-    // Update the board after placing the queen
-    const updatedBoard = [...board];
-    updatedBoard[0][0] = 0;
-    setBoardMock(updatedBoard);
-    rerender(<QueenBoard board={updatedBoard} setBoard={setBoardMock} />);
-
-    // After clicking, expect a queen to not be displayed
+    // Re-render the board
+    rerender(
+      <QueenBoard
+        board={board}
+        handleMouseClickCell={handleMouseClickCellMock}
+      />
+    );
+    // Expect the queen to be now removed
     await waitFor(() => {
       expect(firstCell).not.toHaveTextContent("♛");
     });

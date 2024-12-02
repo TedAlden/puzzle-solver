ef0ebc0254780f54928f1d5da2a5c46628f703e9
Ted <t.aldentempleman@gmail.com>
2024-12-02 14:02:34 +0000

    Update PolyspherePuzzle layout

--- src/pages/PolyspherePuzzle/PolyspherePuzzle.js
+++ src/pages/PolyspherePuzzle/PolyspherePuzzle.js
@@ -36,105 +36,114 @@ function PolyspherePuzzle() {
     handleFlipShape,
   } = usePolyspherePuzzle();
 
+  const keyMap = [
+    {
+      key: "r",
+      keyAlias: "R",
+      description: "Rotate piece",
+      onClick: handleRotateShape,
+    },
+    {
+      key: "f",
+      keyAlias: "F",
+      description: "Flip piece",
+      onClick: handleFlipShape,
+    },
+    {
+      key: "ArrowLeft",
+      keyAlias: "←",
+      description: "Previous piece",
+      onClick: handlePreviousShape,
+    },
+    {
+      key: "ArrowRight",
+      keyAlias: "→",
+      description: "Next piece",
+      onClick: handleNextShape,
+    },
+    {
+      key: "Escape",
+      keyAlias: "Esc",
+      description: "Clear board",
+      onClick: handleClear,
+    },
+    {
+      key: "u",
+      keyAlias: "U",
+      description: "Undo action",
+      onClick: handleUndo,
+    },
+    {
+      key: "s",
+      keyAlias: "S",
+      description: "Solve puzzle",
+      onClick: handleSolve,
+    },
+  ];
+
   return (
     <div className="puzzleTwo">
       <h2>The Polysphere Puzzle</h2>
-      <p>
-        The <b> polysphere puzzle </b> involves placing <b> 12 </b> unique
-        shapes made of connected spheres onto a <b> 5x11 </b> board. Your goal
-        is to fit all pieces perfectly into the grid. Each shape is made from a
-        different configuration of spheres, and you can use the <b> Solve </b>
-        button to find the best way to complete the board.
-      </p>
-      <ProgressBar current={12 - shapes.length} total={12} />
-      {shapes.length > 0 && (
-        <PieceSelector
-          selectedShape={selectedShape}
-          handleFlipShape={handleFlipShape}
-          handlePreviousShape={handlePreviousShape}
-          handleNextShape={handleNextShape}
-          handleRotateShape={handleRotateShape}
-        />
-      )}
-      <PolysphereBoard
-        board={board}
-        highlightedCells={highlightedCells}
-        handleMouseEnterCell={handleMouseEnterCell}
-        handleMouseLeaveCell={handleMouseLeaveCell}
-        handleMouseClickCell={handleMouseClickCell}
-      />
-      <div className="controlsContainer">
-        <button onClick={handleSolve} disabled={isSolving}>
-          {isSolving ? "Solving..." : "Solve Puzzle"}
-        </button>
-        <button onClick={handleClear} disabled={isSolving}>
-          Clear Board
-        </button>
-        <button
-          onClick={handleUndo}
-          disabled={moveStack.length === 0 || isSolving}
-        >
-          Undo
-        </button>
-      </div>
-      <div>
-        {isSolving && <span>Solving ⏳</span>}
-        {isSolved && solutions.length > 0 && <span>Solutions found ✅</span>}
-        {isSolved && solutions.length === 0 && <span>No solutions ⚠️</span>}
-        {solutions.length >= 1 && (
-          <SolutionNavigator
-            solutionIndex={solutionIndex}
-            solutionsLength={solutions.length}
-            handleSetSolutionIndex={handleSetSolutionIndex}
+      <div className="wrapper">
+        <div className="column middleColumn">
+          {shapes.length > 0 && (
+            <PieceSelector
+              selectedShape={selectedShape}
+              handleFlipShape={handleFlipShape}
+              handlePreviousShape={handlePreviousShape}
+              handleNextShape={handleNextShape}
+              handleRotateShape={handleRotateShape}
+            />
+          )}
+          <PolysphereBoard
+            board={board}
+            highlightedCells={highlightedCells}
+            handleMouseEnterCell={handleMouseEnterCell}
+            handleMouseLeaveCell={handleMouseLeaveCell}
+            handleMouseClickCell={handleMouseClickCell}
           />
-        )}
+          <div className="controlsContainer">
+            <button onClick={handleSolve} disabled={isSolving}>
+              {isSolving ? "Solving..." : "Solve Puzzle"}
+            </button>
+            <button onClick={handleClear} disabled={isSolving}>
+              Clear Board
+            </button>
+            <button
+              onClick={handleUndo}
+              disabled={moveStack.length === 0 || isSolving}
+            >
+              Undo
+            </button>
+          </div>
+          <div>
+            {isSolving && <span>Solving ⏳</span>}
+            {isSolved && solutions.length > 0 && (
+              <span>Solutions found ✅</span>
+            )}
+            {isSolved && solutions.length === 0 && <span>No solutions ⚠️</span>}
+            {solutions.length >= 1 && (
+              <SolutionNavigator
+                solutionIndex={solutionIndex}
+                solutionsLength={solutions.length}
+                handleSetSolutionIndex={handleSetSolutionIndex}
+              />
+            )}
+          </div>
+        </div>
+        <div className="column rightColumn">
+          <p className="description">
+            The <b> polysphere puzzle </b> involves placing <b> 12 </b> unique
+            shapes made of connected spheres onto a <b> 5x11 </b> board. Your
+            goal is to fit all pieces perfectly into the grid. Each shape is
+            made from a different configuration of spheres, and you can use the{" "}
+            <b> Solve </b>
+            button to find the best way to complete the board.
+          </p>
+          <ProgressBar current={12 - shapes.length} total={12} />
+          <KeyboardControls keyMap={keyMap} />
+        </div>
       </div>
-      <KeyboardControls
-        keyMap={[
-          {
-            key: "r",
-            keyAlias: "R",
-            description: "Rotate piece",
-            onClick: handleRotateShape,
-          },
-          {
-            key: "f",
-            keyAlias: "F",
-            description: "Flip piece",
-            onClick: handleFlipShape,
-          },
-          {
-            key: "ArrowLeft",
-            keyAlias: "←",
-            description: "Previous piece",
-            onClick: handlePreviousShape,
-          },
-          {
-            key: "ArrowRight",
-            keyAlias: "→",
-            description: "Next piece",
-            onClick: handleNextShape,
-          },
-          {
-            key: "Escape",
-            keyAlias: "Esc",
-            description: "Clear board",
-            onClick: handleClear,
-          },
-          {
-            key: "u",
-            keyAlias: "U",
-            description: "Undo action",
-            onClick: handleUndo,
-          },
-          {
-            key: "s",
-            keyAlias: "S",
-            description: "Solve puzzle",
-            onClick: handleSolve,
-          },
-        ]}
-      />
     </div>
   );
 }

1442bf5142fdda2e593aaa48854ed414e0030e80
Ted <t.aldentempleman@gmail.com>
2024-12-05 13:30:11 +0000

    Made KeyboardControls collapsible

--- src/components/KeyboardControls/KeyboardControls.js
+++ src/components/KeyboardControls/KeyboardControls.js
@@ -1,7 +1,14 @@
+import React, { useState } from "react";
 import "./KeyboardControls.css";
 import useKeyboardInput from "../../hooks/useKeyboardInput";
 
 function KeyboardControls({ keyMap }) {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
   // Create a map of key:onClick functions
   const keyActions = keyMap.reduce((actions, { key, onClick }) => {
     actions[key] = onClick;
@@ -11,15 +18,22 @@ function KeyboardControls({ keyMap }) {
   useKeyboardInput(keyActions);
 
   return (
-    <div className="keyboardControls">
-      <p>Keyboard Controls</p>
-      <ul>
-        {keyMap.map(({ key, keyAlias, description }) => (
-          <li key={key}>
-            {keyAlias ? keyAlias : key} : {description}
-          </li>
-        ))}
-      </ul>
+    <div className={`keyboardControls ${collapsed ? "collapsed" : ""}`}>
+      <div className="header">
+        <span>Keyboard Controls</span>
+        <button onClick={toggleCollapse}>
+          {collapsed ? "Expand" : "Collapse"}
+        </button>
+      </div>
+      {!collapsed && (
+        <ul>
+          {keyMap.map(({ key, keyAlias, description }) => (
+            <li key={key}>
+              {keyAlias ? keyAlias : key} : {description}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }

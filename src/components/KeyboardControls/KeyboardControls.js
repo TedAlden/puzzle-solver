ff7c4f67225703479236071e0755d741170ad7fd
Ted <t.aldentempleman@gmail.com>
2024-12-06 13:28:53 +0000

    Add JSDoc for KeyboardControls

--- src/components/KeyboardControls/KeyboardControls.js
+++ src/components/KeyboardControls/KeyboardControls.js
@@ -2,9 +2,20 @@ import React, { useState } from "react";
 import "./KeyboardControls.css";
 import useKeyboardInput from "../../hooks/useKeyboardInput";
 
+/**
+ * Displays a list of keyboard controls.
+ *
+ * @param {Object} props Component properties.
+ * @param {Array} props.keyMap The key map object. Contains key, keyAlias, and
+ *  description.
+ * @returns {React.JSX.Element}
+ */
 function KeyboardControls({ keyMap }) {
   const [collapsed, setCollapsed] = useState(false);
 
+  /**
+   * Toggles the collapsed state of the keyboard controls.
+   */
   const toggleCollapse = () => {
     setCollapsed(!collapsed);
   };

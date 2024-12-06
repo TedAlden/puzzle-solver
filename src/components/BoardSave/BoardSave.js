c993ae799fb12972ac82d95544ac27b95203e954
Ted <t.aldentempleman@gmail.com>
2024-12-06 10:56:59 +0000

    Add JSDoc for BoardSave

--- src/components/BoardSave/BoardSave.js
+++ src/components/BoardSave/BoardSave.js
@@ -1,20 +1,36 @@
 import React from "react";
 
+/**
+ * Buttons to save the current board as a JSON or CSV file.
+ *
+ * @param {Object} props Component properties.
+ * @param {Array<Array<string>>} props.board The current board state.
+ * @returns {React.JSX.Element}
+ */
 const BoardSave = ({ board }) => {
+  /**
+   * Exports the current board as a JSON file.
+   */
   const handleExportAsJSON = () => {
-    const placedPieces = board.flatMap((layer, layerIndex) =>
-      layer.flatMap((row, rowIndex) =>
-        row.map((cell, colIndex) => {
-          if (cell) {
-            return {
-              piece: cell,
-              position: { layer: layerIndex, row: rowIndex, column: colIndex },
-            };
-          }
-          return null;
-        })
+    const placedPieces = board
+      .flatMap((layer, layerIndex) =>
+        layer.flatMap((row, rowIndex) =>
+          row.map((cell, colIndex) => {
+            if (cell) {
+              return {
+                piece: cell,
+                position: {
+                  layer: layerIndex,
+                  row: rowIndex,
+                  column: colIndex,
+                },
+              };
+            }
+            return null;
+          })
+        )
       )
-    ).filter(Boolean);
+      .filter(Boolean);
 
     const boardState = { board, placedPieces };
     const jsonBlob = new Blob([JSON.stringify(boardState, null, 2)], {
@@ -26,12 +42,19 @@ const BoardSave = ({ board }) => {
     link.click();
   };
 
+  /**
+   * Exports the current board as a CSV file with the following format:
+   * ```
+   * (Layer, Row, Column, Piece)
+   * ```
+   */
   const handleExportAsCSV = () => {
     const header = "Layer,Row,Column,Piece\n";
     const rows = board.flatMap((layer, layerIndex) =>
       layer.flatMap((row, rowIndex) =>
-        row.map((cell, colIndex) => 
-          `${layerIndex},${rowIndex},${colIndex},${cell || ""}`
+        row.map(
+          (cell, colIndex) =>
+            `${layerIndex},${rowIndex},${colIndex},${cell || ""}`
         )
       )
     );
